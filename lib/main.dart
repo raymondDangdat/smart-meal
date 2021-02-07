0410fc6c3a7d5a0409ad4f29ea32f953caeb1b76
Dangdat <raymonddangdat@gmail.com>
2021-02-07 07:39:58 +0100

    Starting point of Smart Meal App

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/screens/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meal App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'RobotoCondensed',
+          )
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/widgets/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Smart Meal"),
+        centerTitle: true,
+      ),
+        body: GridView(
+          padding: const EdgeInsets.all(15.0),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),  children: DUMMY_CATEGORIES.map((categoryData) => CategoryItem(categoryData.title, categoryData.color)).toList(),),
+    );
+  }
+}
