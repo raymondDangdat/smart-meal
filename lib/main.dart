a6d699e55d834ef888444547c89c657e3ce07a7b
Dangdat <raymonddangdat@gmail.com>
2021-02-08 17:15:00 +0100

    Implementd filtering logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/bottom_tab_screen.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import './models/meal.dart';
+import './screens/bottom_tab_screen.dart';
 import './screens/filters_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_details_screen.dart';
@@ -8,8 +10,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData){
+    setState(() {
+      _filters = filterData;
+      _availableMeals = DUMMY_MEALS.where((meal){
+        if(_filters['gluten'] && !meal.isGlutenFree){
+           return false;
+        }
+        if(_filters['lactose'] && !meal.isLactoseFree){
+          return false;
+        }
+        if(_filters['vegan'] && !meal.isVegan){
+          return false;
+        }
+        if(_filters['vegetarian'] && !meal.isVegetarian){
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,14 +70,14 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       routes: {
         '/' : (context) => BottomTabScreen(),
-        CategoryMealScreen.routeName : (context) => CategoryMealScreen(),
+        CategoryMealScreen.routeName : (context) => CategoryMealScreen(_availableMeals),
         MealDetailsScreen.routeName : (context) => MealDetailsScreen(),
-        FilterScreen.routeName : (context) => FilterScreen(),
+        FilterScreen.routeName : (context) => FilterScreen(_filters, _setFilters),
       },
 
     //  GIVE A FALL BACK ROUTE INCASE SOMETHING WENT WRONG
       onUnknownRoute: (settings){
-        return MaterialPageRoute(builder: (context) => CategoryMealScreen());
+        return MaterialPageRoute(builder: (context) => CategoryMealScreen(_availableMeals));
     },
     );
   }
